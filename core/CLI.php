154654774adb9e118164adfba45f73eddea7b325
Bagus A Priambada <iam.adty@gmail.com>
2020-11-17 18:38:10 +0700

    use pwc static config

--- core/CLI.php
+++ core/CLI.php
@@ -1,11 +1,4 @@
 <?php namespace PWC;
 
-class CLI extends \GetOpt\Command {
-    protected $_config = [];
-
-    public function setConfig($config = [])
-    {
-        $this->_config = $config;
-        return $this;
-    }
-}
+class CLI extends \GetOpt\Command
+{}
---
+++ src/Config.php
@@ -0,0 +1,6 @@
+<?php namespace PWC\CLI;
+
+class Config extends \PWC\Singleton\Config
+{
+    protected $composerAutoload = null;
+}
